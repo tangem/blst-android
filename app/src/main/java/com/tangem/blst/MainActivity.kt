4c83798f318cbe3c7c7de382a3d6693af971891c
Vladislav Kozarez <vladks32@gmail.com>
2023-07-27 13:02:21 +0300

    build blst lib

--- app/src/main/java/com/tangem/blst/MainActivity.kt
+++ app/src/main/java/com/tangem/blst/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.tangem.blst
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.tangem.blst.databinding.ActivityMainBinding
+import com.tangem.blstlib.generated.P2
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,20 +15,21 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        P2.generator().hash_to(byteArrayOf(Byte.MAX_VALUE))
         // Example of a call to a native method
-        binding.sampleText.text = stringFromJNI()
+//        binding.sampleText.text = stringFromJNI()
     }
 
     /**
      * A native method that is implemented by the 'blst' native library,
      * which is packaged with this application.
      */
-    external fun stringFromJNI(): String
+//    external fun stringFromJNI(): String
 
-    companion object {
-        // Used to load the 'blst' library on application startup.
-        init {
-            System.loadLibrary("blst")
-        }
-    }
+//    companion object {
+//        // Used to load the 'blst' library on application startup.
+//        init {
+//            System.loadLibrary("blst")
+//        }
+//    }
 }
